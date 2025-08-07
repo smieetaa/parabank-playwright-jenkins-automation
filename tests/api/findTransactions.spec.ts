1d83d4e5968393d71ba05f9869d62f2908c9e4f9
smieetaa <92695563+smieetaa@users.noreply.github.com>
2025-08-07 12:48:29 +0000

    converted to test suite

---
+++ tests/api/findTransactions.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('Find transaction by amount', async ({ request }) => {
+//   const response = await request.get('https://parabank.parasoft.com/parabank/services/bank/findTransactions?accountId=12345&amount=100');
+//   expect(response.ok()).toBeTruthy();
+
+//   const json = await response.json();
+//   expect(Array.isArray(json)).toBe(true);
+});
